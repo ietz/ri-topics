cdffe9ae36e99f2c4ed655b448923f657656942c
Tim Pietz <tim@pietz.me>
2019-12-17 19:55:12 +0100

    Test topic model manager orchestration of topic models

--- tests/test_topics.py
+++ tests/test_topics.py
@@ -8,7 +8,7 @@ import numpy as np
 from ri_topics.clustering import Clusterer, ClusterAssignment
 from ri_topics.embedder import Embedder
 from ri_topics.openreq.ri_storage_twitter import RiStorageTwitter, Tweet
-from ri_topics.topics import TopicModel
+from ri_topics.topics import TopicModel, TopicModelManager
 from ri_topics.util import mock_dataclass_asdict
 
 embedding_dim = 768
@@ -75,6 +75,85 @@ class TestTopicModel(unittest.TestCase):
         # Update
         topic_model.update(embedder, storage)
         self.assertSetEqual({'0', '1', '2', '3', '4', '5'}, set(topic_model.tweet_df.index))
+
+
+class TestTopicModelManager(unittest.TestCase):
+    @mock.patch('ri_topics.topics.pickle')
+    @mock.patch('ri_topics.topics.TopicModel')
+    @mock.patch.object(TopicModelManager, '_path')
+    def test_create_model(self, mock_manager_path, MockTopicModel, mock_pickle):
+        account_name = 'A'
+
+        embedder = Mock(spec=Embedder)
+        storage = Mock(spec=RiStorageTwitter, **{
+            'get_all_account_names.return_value': [account_name],
+        })
+
+        MockTopicModel.return_value = Mock(**{
+            'account_name': account_name,
+        })
+        mock_manager_path.return_value = Mock(**{
+            'exists.return_value': False,
+            'open': mock.mock_open(),
+        })
+
+        manager = TopicModelManager(embedder, storage)
+        manager.prepare_all()
+        self.assertEqual(0, mock_pickle.load.call_count)
+        self.assertEqual(1, mock_pickle.dump.call_count)
+
+    @mock.patch('ri_topics.topics.pickle')
+    @mock.patch('ri_topics.topics.TopicModel')
+    @mock.patch.object(TopicModelManager, '_path')
+    def test_load_model(self, mock_manager_path, MockTopicModel, mock_pickle):
+        account_name = 'A'
+
+        embedder = Mock(spec=Embedder)
+        storage = Mock(spec=RiStorageTwitter, **{
+            'get_all_account_names.return_value': [account_name],
+        })
+
+        MockTopicModel.return_value = Mock(**{
+            'account_name': account_name,
+        })
+        mock_manager_path.return_value = Mock(**{
+            'exists.return_value': True,
+            'open': mock.mock_open(),
+        })
+        mock_pickle.configure_mock(**{
+            'load.return_value': MockTopicModel(),
+        })
+
+        manager = TopicModelManager(embedder, storage)
+        manager.prepare_all()
+        self.assertEqual(1, mock_pickle.load.call_count)
+        self.assertEqual(0, mock_pickle.dump.call_count)
+
+    @mock.patch('ri_topics.topics.pickle')
+    @mock.patch('ri_topics.topics.TopicModel')
+    @mock.patch.object(TopicModelManager, '_path')
+    def test_update(self, mock_manager_path, MockTopicModel, mock_pickle):
+        topic_models = {name: Mock(account_name=name) for name in ['A', 'B', 'C']}
+
+        MockTopicModel.side_effect = lambda account_name: topic_models[account_name]
+        mock_manager_path.side_effect = lambda account_name: Mock(**{
+            'exists.return_value': True,
+            'open': mock.mock_open(read_data=account_name),
+        })
+        mock_pickle.configure_mock(**{
+            'load.side_effect': lambda f: topic_models[f.read()],
+        })
+
+        embedder = Mock(spec=Embedder)
+        storage = Mock(spec=RiStorageTwitter, **{
+            'get_all_account_names.return_value': list(topic_models.keys()),
+        })
+
+        manager = TopicModelManager(embedder, storage)
+        manager.update_all()
+        self.assertTrue(all([model.update.called for model in topic_models.values()]))
+        self.assertEqual(len(topic_models), mock_pickle.load.call_count)
+        self.assertEqual(len(topic_models), mock_pickle.dump.call_count)
 
 
 if __name__ == '__main__':
